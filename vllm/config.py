95e505f473575da1c22e648642d3c0d9e5a7e84d
Michael Goin <michael@neuralmagic.com>
2025-02-06 02:16:20 -0500

    [Models] Support DeepSeekV3.

--- vllm/config.py
+++ vllm/config.py
@@ -388,6 +388,13 @@ class ModelConfig:
     def get_hidden_size(self) -> int:
         return self.hf_text_config.hidden_size
 
+    @property
+    def is_deepseek_mla(self) -> bool:
+        return (hasattr(self.hf_text_config, "model_type")) \
+                and (self.hf_text_config.model_type in \
+                    ('deepseek_v2', 'deepseek_v3'))\
+                and (self.hf_text_config.kv_lora_rank is not None)
+
     def get_head_size(self) -> int:
         # TODO remove hard code
         if hasattr(self.hf_text_config, "model_type"
